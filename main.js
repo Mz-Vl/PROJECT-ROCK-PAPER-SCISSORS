9c568083757d5d3ad1d6bb2c2d0c2d8be7c2bdbb
Vladyslav Mazur <vladyslav.mazur.5@gmail.com>
2021-04-25 22:39:38 +0300

    Add gameResult and game functions

--- main.js
+++ main.js
@@ -1,39 +1,78 @@
-const computerAanswer = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
 
 function computerPlay() {
+    const computerAanswer = ["rock", "paper", "scissors"];
     let answer = computerAanswer[Math.floor(Math.random() * computerAanswer.length)];
-    console.log('computer choice is: ' + answer.toUpperCase()); 
+    console.log('Computer choice is: ' + answer.toUpperCase()); 
     return answer;
 }
 
 
 
-function playRound (playerSelection, computerSelection) {
-    playerSelection  = playerSelection.toLowerCase()
+function playRound () {
+    let playerSelection = prompt("Make your choose: Rock, Paper or Scissors?").toLowerCase();
+    let computerSelection = computerPlay();
     
     if  ((playerSelection == "rock"     && computerSelection == "scissors") ||
          (playerSelection == "paper"    && computerSelection == "rock")     ||
          (playerSelection == "scissors" && computerSelection == "paper")) {
-
-            return "Congrats,you are winner!";
+         playerScore++;
+         let roundResult = 'Your choice is: ' + playerSelection.toUpperCase();
+         roundResult += "\nCongrats,you are winner!";
+         roundResult += '\nYour score is: ' + playerScore;
+         roundResult += '\nComputer score is: ' + computerScore;
+                 return roundResult;
 
     }   else if ((playerSelection == "rock"     && computerSelection == "rock")    ||
                  (playerSelection == "paper"    && computerSelection == "paper")   ||
                  (playerSelection == "scissors" && computerSelection == "scissors")) {
+                 let roundResult = 'Your choice is: ' + playerSelection.toUpperCase();
+                 roundResult += "\nDraw!";
+                 roundResult += '\nYour score is: ' + playerScore;
+                 roundResult += '\nComputer score is: ' + computerScore;
+                 return roundResult;
+                 
 
-            return "Draw!";
     }   else if ((computerSelection == "rock"     && playerSelection == "scissors")  ||
                  (computerSelection == "paper"    && playerSelection == "rock")      ||
                  (computerSelection == "scissors" && playerSelection == "paper")){
+                 computerScore++;
+                 let roundResult = 'Your choice is: ' + playerSelection.toUpperCase();
+                 roundResult += "\nYou lose. Maybe next time...";
+                 roundResult += '\nYour score is: ' + playerScore;
+                 roundResult += '\nComputer score is: ' + computerScore;
+                 return roundResult;
 
-            return "You lose. Maybe next time...";
     }   else {
             return 'Wrong input.'
     }
 }
 
-let playerSelection = prompt("Make your choose: Rock, Paper or Scissors?");
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function gameResult() {
+    if (playerScore > computerScore) {
+        console.log('You won this game! ');
+    } else if (computerScore > playerScore) {
+        console.log('You lost!');
+    } else if (computerScore == playerScore) {
+        console.log('Wow! It\'s Draw!')
+    }
+}
 
-console.log('player choice is: ' + playerSelection.toUpperCase());
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
+    }
+    return gameResult();
+}
+
+// function game() {
+//     console.log(playRound());
+//     if (playerScore < 5 && computerScore < 5) {
+//         game();
+//     } else {
+//         gameResult();
+//     }
+// }
+
+game();
